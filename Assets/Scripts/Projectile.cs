8dbee4464c25c0fa5e6fa24ab58e3b81542b5b52
Elijah Hewer <phenomhewer@gmail.com>
2020-08-17 11:38:55 -0700

    First commit

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    Rigidbody2D rigidbody2D;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        if(transform.position.magnitude > 1000.0f){
+            Destroy(gameObject);
+        }
+    }
+
+    public void Launch(Vector2 direction, float force) {
+        rigidbody2D.AddForce(direction * force);
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+        EnemyController e = other.collider.GetComponent<EnemyController>();
+
+        if (e != null) {
+            e.Fix();
+        }
+
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/RubyController.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubyController : MonoBehaviour
+{
+    public int maxHealth = 3;
+    int currentHealth;
+    public int health {
+        get {
+            return currentHealth;
+        }
+    }
+
+    public float speed = 3.0f;
+    Rigidbody2D rigidbody2d;
+    float horizontal; 
+    float vertical;
+
+    public float timeInvincible = 2.0f;
+    bool isInvincible;
+    float invincibleTimer;
+
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1,0);
+
+    public GameObject projectilePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // QualitySettings.vSyncCount = 0;
+        // Application.targetFrameRate = 60;
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+        
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+                
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
+        if (isInvincible) {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer <= 0) {
+                isInvincible = false;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.C)) {
+            Launch();
+        }
+    }
+
+    void FixedUpdate() {
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+
+
+        rigidbody2d.MovePosition(position);
+    }
+
+    public void ChangeHealth(int amount) {
+        if (amount < 0) {
+            if (isInvincible) return;
+
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+            animator.SetTrigger("Hit");
+        }
+
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
+    void Launch() {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f,
+            Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
+
+        animator.SetTrigger("Launch");
+    }
+}
